e77b9c3ec5dcd1a0aaaf85097f0d66b7dff8e3e9
Tiago Neves <rykarumstuler@gmail.com>
2021-03-26 20:00:41 -0300

    Codificando a dinamicidade na primeira atividade

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.happybirthday
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.dice_roler)
+
+        val rollButton: Button = findViewById(R.id.button)
+
+        rollButton.setOnClickListener {
+            val resultTextView: TextView = findViewById(R.id.resultText)
+            resultTextView.text = "6"
+
+        }
     }
 }
