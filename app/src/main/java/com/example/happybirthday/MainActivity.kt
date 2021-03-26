c9d91b1113ab7fdb81326c0aea1345ec62231342
Tiago Neves <rykarumstuler@gmail.com>
2021-03-26 20:30:27 -0300

    Criado o script de randonizar o número que aparece, quando o botão for clicado

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -9,27 +9,32 @@ import android.widget.Toast
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // Seta o conteúdo da view utilizando o layout que eu escolher
         setContentView(R.layout.dice_roler)
 
+        // Cria um ponto da memória para localizar a view pelo id único
         val rollButton: Button = findViewById(R.id.button)
 
-        rollButton.setOnClickListener {
-            rollDice()
-        }
+        // Chama a fução rollDice quando o botão referenciado for clicado
+        rollButton.setOnClickListener { rollDice() }
     }
-//    FUNÇÕES
+
+    //    FUNÇÕES
 //    Função de rodar o dado
     private fun rollDice() {
+        // Cria um objeto dice e chama o método roll()
         val dice = Dice(6)
         val diceRoll = dice.roll()
-        val resultTextView: TextView = findViewById(R.id.resultText)
 
+        // Cria um ponto na memória para view e modifica o text atribuindo o valor do método roll.
+        val resultTextView: TextView = findViewById(R.id.resultText)
         resultTextView.text = diceRoll.toString()
     }
 
-//    CLASSES
+    //    CLASSES
 //   Criação da classe Dado
     class Dice(private val numSides: Int) {
+        // Cira um método chamado roll, que retorna um valor aleatório em um arranjo
         fun roll(): Int {
             return (1..numSides).random()
         }
